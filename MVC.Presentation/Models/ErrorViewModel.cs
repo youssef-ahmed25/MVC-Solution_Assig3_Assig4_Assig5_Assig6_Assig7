2420d5ca3b2a5f4f71d3763eddc4e88b5bd61843
youssef-ahmed25 <youssefahmed56432@gmail.com>
2025-09-12 00:20:34 +0300

    Add project files.

---
+++ MVC.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
